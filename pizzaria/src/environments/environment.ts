d50eebb076b3445b0b5ba933f895c387ce11d9a3
João Sousa <jgsoaressousa772@outlook.com>
2024-11-27 11:06:11 -0300

    servicos

---
+++ pizzaria/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDJmcm1dLx4-jpgg7nUHM2XgUElvQx0Dj8",
+        authDomain: "pizzaria-a305e.firebaseapp.com",
+        projectId: "pizzaria-a305e",
+        storageBucket: "pizzaria-a305e.firebasestorage.app",
+        messagingSenderId: "198592109960",
+        appId: "1:198592109960:web:ed53814341f3e6de1b2e3d"
+    }
+  };
+  
